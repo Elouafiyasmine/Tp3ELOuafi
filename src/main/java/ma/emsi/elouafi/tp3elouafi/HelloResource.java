026792e5b9019f3b28538b2a8f5315add67625c7
tp0elouafi <elouafi.yasmine12@gmail.com>
2025-11-08 16:50:10 +0100

    modificqtion du resources

--- src/main/java/ma/emsi/elouafi/tp3elouafi/HelloResource.java
+++ src/main/java/ma/emsi/elouafi/tp3elouafi/HelloResource.java
@@ -4,11 +4,19 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
+import jakarta.ws.rs.PathParam;
+
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
     public String hello() {
         return "Hello, World!";
+    }
+    @GET
+    @Path("personnes/{nom}")
+    @Produces("text/plain")
+    public String helloPersonne(@PathParam("nom") String nom) {
+        return "Hello, " + nom + "!";
     }
 }
