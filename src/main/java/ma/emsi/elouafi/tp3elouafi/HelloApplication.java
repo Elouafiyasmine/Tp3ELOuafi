c4e5a472b20ebc0ed4a0d9cedfdca467a93e14b0
tp0elouafi <elouafi.yasmine12@gmail.com>
2025-11-08 16:38:09 +0100

    Creation du Tp

---
+++ src/main/java/ma/emsi/elouafi/tp3elouafi/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.elouafi.tp3elouafi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
